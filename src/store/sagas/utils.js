1689d527ebc0a5101a405ca3cce1d121c201112b
Unknown <fabricio@asap.uy>
2017-10-09 09:45:59 -0300

    Sagas handle error

--- src/store/sagas/utils.js
+++ src/store/sagas/utils.js
@@ -1,8 +1,26 @@
 export const fetchJSON = (url, options = {}) =>
     new Promise((resolve, reject) => {
         return fetch(url, options)
-            .then(response => (response.status !== 200 ? reject(response) : response))
-            .then(response => response.json())
+            .then(response => (response.status !== 200 && response.status !== 201 ? reject(response) : response))
+            .then(response => response.json().catch(err => { return {}; }))
             .then(response => resolve(response))
             .catch(error => reject(error));
     });
+
+export const handleError = (status) => {
+    let message = "";
+    switch (status) {
+        case 500:
+            message = 'Internal Server Error';
+            break;
+        case 401:
+            message = 'Invalid credentials';
+            break;
+        case 403:
+            message = 'Invalid credentials';
+            break;
+        default:
+            message = 'Something went wrong';
+    }
+    return message;
+}
